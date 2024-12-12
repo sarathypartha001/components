482df6fbfe18b626ec1042797be6089b971ba026
sarathypartha001 <parthasarathya001@gmail.com>
2024-12-12 19:02:17 +0530

    modified files

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,22 @@ import "./index.css";
 
 function App(){
     return(
-        <>
+        <div>
         <h1 className="App-main-head">Company Details</h1>
         <Company companyName="Microsoft" details="Microsoft is a product based company"/>
         <Company companyName="Zoho" details="ZOHO is a product based company"/>
         <Company companyName="CTS" details="TCS is a service based company"/>
         <Company />
-        </>
+        
+        
+        
+        
+        {/* <Company>
+            <div>
+                <h2>hi this is parthasarathy</h2>
+            </div>
+        </Company> */}
+        </div>
     );
 }
 export default App;
--- src/company/company.jsx
+++ src/company/company.jsx
@@ -1,18 +1,22 @@
+
 import "./company.css";
 
 
 function Company(props){
+    const {companyName= 'Unknown Company' , details= 'No information avalable', /*children*/} =props;
+    // const buttonClick =(companyName)=>{
+    //     console.log("selectone"+ companyName)
+    // }
     return(
-        <>
-        <h1 className="Company-head">Company Name : {props.companyName}</h1>
-        <p className="company-details">{props.details}</p>
-        </>
+        <div className="details-main">
+        <h1 className="Company-head">Company Name : {companyName}</h1>
+        <p className="company-details">{details}</p>
+        <button className="primary-btn">Click Button</button>
+    
+         {/* <button onClick={()=>{buttonClick(companyName)}}>Select</button>  */}
+        {/* {children} */}
+        </div>
     );
 }
-
-Company.defaultProps = {
-    companyName : "unknown company",
-    details : "No Information Availabe"
-};
 
 export default Company;
